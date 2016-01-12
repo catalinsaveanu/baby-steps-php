1c0e60eeb0fa6404374bd0d7387e4aa81ad7010a
Catalin Saveanu <catalinsaveanu@gmail.com>
2016-01-13 00:03:45 +0200

    add MVC

---
+++ app/controllers/home.php
@@ -0,0 +1,20 @@
+<?php
+class Home extends Controller {
+	/**
+	 * @param string $name
+	 * @param string $mood
+	 */
+	public function index($name = 'cata', $mood = 'happy') {
+		$user = $this->model('User');
+		$user->name = $name;
+
+		$this->view('home/index', [
+			'name' => $user->name,
+			'mood' => $mood
+		]);
+	}
+
+	public function test() {
+		echo 'test';
+	}
+}
---
+++ app/views/home/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Simple MVC</title>
+
+	</head>
+	<body>
+		<p>Welcome to the home/index</p>
+		<p>Bellow the results passed to the url</p>
+		<p><?= $data['name'] ?> is <?= $data['mood'] ?></p>
+	</body>
+</html>
